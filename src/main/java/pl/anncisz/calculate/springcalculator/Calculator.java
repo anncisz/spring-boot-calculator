74b71b07b7d4c2bc6f9e759df02a1be3827d85db
anncisz <anncisz@gmail.com>
2019-05-25 11:40:39 +0200

    first commit

---
+++ src/main/java/pl/anncisz/calculate/springcalculator/Calculator.java
@@ -0,0 +1,41 @@
+package pl.anncisz.calculate.springcalculator;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+    private BiFunction<Double, Double, Double> add = (a, b) -> a + b;
+    private BiFunction<Double, Double, Double> subtract = (a, b) -> a - b;
+    private BiFunction<Double, Double, Double> divide = (a, b) -> a / b;
+    private BiFunction<Double, Double, Double> multiply = (a, b) -> a * b;
+
+    private Map<String, BiFunction<Double, Double, Double>> functionMap = mapBuilding();
+
+    private Map<String, BiFunction<Double, Double, Double>> mapBuilding() {
+        Map<String, BiFunction<Double, Double, Double>> result = mapBuilding();
+        result.put("+", add);
+        result.put("-", subtract);
+        result.put("*", multiply);
+        result.put("/", divide);
+        return result;
+    }
+
+    public Double calculate(String input) {
+        Pattern pattern = Pattern.compile("(\\d+)(\\D+)(\\d+)");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            Double firstValue = Double.valueOf(matcher.group(1));
+            String sign = matcher.group(2).trim();
+            Double secondValue = Double.valueOf(matcher.group(3));
+
+            return functionMap.get(sign).apply(firstValue, secondValue);
+
+        }
+        return null;
+    }
+
+
+}
---
+++ src/test/java/pl/anncisz/calculate/springcalculator/SpringCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.anncisz.calculate.springcalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCalculatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
